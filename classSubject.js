a66aead6a652923a8567de2542a85261db93cd84
cjdelacruz <cjdelacruz0521@gmail.com>
2024-11-04 17:54:26 +0700

    Initial commit with classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+//Collect the following user information:
+
+let subjectTitle = "Art Appreciation";
+let classSched = "9:30- 11:00, Tuesday, Thursday";
+let classroom = "room123"
+let classInstructor = "Sir Marquez";
+
+console.log(' Cassandra Jade H. Delacruz is enrolled in Art Appreciation with a class schedule of 9:30- 11:00, Tuesday, Thursday, at room123.The instructor for the subject is Sir Marquez ');
